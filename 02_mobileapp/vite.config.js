7e8e1eac99ad502d85ac3928189b5afce170a484
384felix <fesil1@hotmail.de>
2026-01-16 22:37:47 +0100

    Fix Vite base for GitHub Pages

--- 02_mobileapp/vite.config.js
+++ 02_mobileapp/vite.config.js
@@ -1,20 +1,18 @@
-
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
-
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www',);
-export default async () => {
+const BUILD_DIR = path.resolve(__dirname, './www');
 
-  return  {
-    plugins: [
-      react(),
-
-    ],
+export default async ({ command }) => {
+  return {
+    plugins: [react()],
     root: SRC_DIR,
-    base: '',
+
+    // ✅ KORREKT für DevMobileApplications
+    base: command === 'serve' ? '/' : '/DevMobileApplications/',
+
     publicDir: PUBLIC_DIR,
     build: {
       outDir: BUILD_DIR,
@@ -32,6 +30,5 @@ export default async () => {
     server: {
       host: true,
     },
-
   };
-}
+};
